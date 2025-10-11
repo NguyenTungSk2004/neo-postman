10707a9ac20b91b6d66671817291b6aa3a52b916
tungsk <skfreelancer2004@gmail.com>
2025-10-11 15:14:14 +0700

    update project webapi

--- CreateSln.ps1
+++ CreateSln.ps1
@@ -2,6 +2,7 @@
 dotnet new sln -n $YourProjectName --force
 
 dotnet sln add "BlazorWebApp/BlazorWebApp.csproj"
+dotnet sln add "WebApi/WebApi.csproj"
 dotnet sln add "Application/Application.csproj"
 dotnet sln add "Domain/Domain.csproj"
 dotnet sln add "Infrastructure/Infrastructure.csproj"

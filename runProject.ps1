4c169cb5b4ef23e0950b42317f9abefa408297c5
tungsk <skfreelancer2004@gmail.com>
2025-10-14 18:08:58 +0700

    update run blazor

---
+++ runProject.ps1
@@ -0,0 +1,2 @@
+Start-Process powershell -ArgumentList "dotnet watch run --project WebApi/WebApi.csproj"
+Start-Process powershell -ArgumentList "dotnet watch run --project BlazorWebApp/BlazorWebApp.csproj"
